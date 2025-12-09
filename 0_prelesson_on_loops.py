833a7fffccbc4b50bb60f2f84b349de4644a1d7b
bobjackie <jackie.aug31@gmail.com>
2025-12-09 20:43:15 +0000

    loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,28 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# while loops  
+# for loops = execute of code a fixed number of times 
+    # You can iterate over a r ange, string and sequence etc.
+
+ credit_card = "2318-2132-2132-5434"
+for x in credit_card:
+   print(x)
+for x in reversed(range(1, 11, 2)):
+   # print(x)
+#for x in range(1, 11, 2):
+   # print(x)
+# print("Happy New Year")
+
+for x in range(1, 21):
+    if x == 13:
+        continue
+    else:
+        print(x)
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
+
+
+        
--- 1_loops.py
+++ 1_loops.py
@@ -1,7 +1,7 @@
 # Example Practice:
 # Given this list of fruits:
 fruits = ["apple", "banana", "cherry", "date"]
-
+print(len(fruits))
 # Challenge:
 # Use a for loop to print each fruit on a new line.
 print(fruits[0])
@@ -9,19 +9,43 @@ print(fruits[1])
 print(fruits[2])
 for fruit in fruits:
     print(fruit)
+
 # i just worked with loops
 
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
+
+print(len(subjects))
+for subject in subjects:
+    print(subject)
+
+for subject in subjects:
+    if subjects == "Science":
+        continue
+    print(subject)
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
-
+for index in range(len(subjects)):
+    print("Subject" + str(index) + ":" + subjects[index])
 
 # Given:
 numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+
+total = 0
+for number in numbers:
+    total += number 
+print("total:" , total) 
+
+
+new_numbers = list(range(1, 600001))
+tato=0
+for new_number in new_numbers:
+    tato += new_number 
+print("tato:" , tato) 
+
